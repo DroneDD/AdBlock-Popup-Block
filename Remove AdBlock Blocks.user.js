b0347a15dba7e3e1f4d80a7499a2f1032f2533b9
DroneDD <52835817+DroneDD@users.noreply.github.com>
2022-06-01 16:20:50 -0400

    Fix unnecessary scrollbar "fixes"

--- Remove AdBlock Blocks.user.js
+++ Remove AdBlock Blocks.user.js
@@ -16,7 +16,10 @@ divs.forEach(function(d) {
     var s = window.getComputedStyle(d, null);
     if (s.top == "0px" && s.left == "0px" && s.bottom == "0px" && s.right == "0px" && s.position == "fixed") x = d;
 });
-document.querySelector("[style*=overflow]").style.setProperty("overflow", "scroll");
-var parent = x.parentElement;
-if (!(parent === document.body)) x.parentNode.remove();
-else x.remove();
+
+if (x){
+	document.querySelector("[style*=overflow]").style.setProperty("overflow", "scroll");
+	var parent = x.parentElement;
+	if (!(parent === document.body)) x.parentNode.remove();
+	else x.remove();
+}
